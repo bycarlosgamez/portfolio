be129b120968aff3b79f5c4c3a9c6c8094688e3a
Carlos Gámez <hardpx@gmail.com>
2024-01-14 17:29:04 -0600

    added modal message while page loading

--- js/app.js
+++ js/app.js
@@ -1,23 +1,30 @@
-/* ******** Menu ******** */
+window.addEventListener('load', () => {
+  const messageOnLoad = document.getElementById('loadingMessage');
+  setTimeout(() => {
+    messageOnLoad.classList.add('message-hidden');
+  }, 4000);
+})(
+  /* ******** Menu ******** */
 
-((d) => {
-  const $btnMenu = d.querySelector('.menu-btn');
-  $menu = d.querySelector('.menu');
+  (d) => {
+    const $btnMenu = d.querySelector('.menu-btn');
+    $menu = d.querySelector('.menu');
 
-  $btnMenu.addEventListener('click', (e) => {
-    $btnMenu.firstElementChild.classList.toggle('none');
-    $btnMenu.lastElementChild.classList.toggle('none');
-    $menu.classList.toggle('is-active');
-  });
+    $btnMenu.addEventListener('click', (e) => {
+      $btnMenu.firstElementChild.classList.toggle('none');
+      $btnMenu.lastElementChild.classList.toggle('none');
+      $menu.classList.toggle('is-active');
+    });
 
-  d.addEventListener('click', (e) => {
-    if (!e.target.matches('.menu a')) return false;
+    d.addEventListener('click', (e) => {
+      if (!e.target.matches('.menu a')) return false;
 
-    $btnMenu.firstElementChild.classList.remove('none');
-    $btnMenu.lastElementChild.classList.add('none');
-    $menu.classList.remove('is-active');
-  });
-})(document);
+      $btnMenu.firstElementChild.classList.remove('none');
+      $btnMenu.lastElementChild.classList.add('none');
+      $menu.classList.remove('is-active');
+    });
+  }
+)(document);
 
 /* ******** Form ******** */
 ((d) => {
